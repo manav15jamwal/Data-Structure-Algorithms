97da14ffc9fd3014a43d54868a4f1b2b8e0a82c8
manav15jamwal <manav15jamwal@gmail.com>
2026-08-03 12:27:11 +0530

    Leetcode 392

---
+++ Leetcode/Is Subsequence.py
@@ -0,0 +1,13 @@
+# Goal to check if one candidate string is sub sequence of another string i.e Basically one string has all characters of other string
+# Approach : Two pointer one points the candidate key ' character other searchs for it in entire string
+
+def is_subsequence(str1,str2):
+    fast,slow = 0,0
+    while fast < len(str2):
+        if str1[slow] == str2[fast]:
+            slow+=1
+            if slow == len(str1):
+                return True
+        fast+=1
+    return False
+print(is_subsequence("abc","abnnnhc"))
