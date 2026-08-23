42967f6726a19b230fbd1fb43928a24822049e28
manav15jamwal <manav15jamwal@gmail.com>
2026-08-23 23:52:59 +0530

    Leetcode 443

--- Leetcode/Compress_string.py
+++ Leetcode/Compress_string.py
@@ -1,43 +1,31 @@
+# Goal : To compress the string if character repeats more than one and less than 10 , write it as "a","9" , 
+# but  if exceeds 10; "a","1","2", if it does not repeat simply write a
+# Approach: Use a set to check if same character is repeating or another:
+# when character changes append the count in result
+# in case of repeating char till end of the string use elif condition for incrementing cound and adding it to result
+# in else block increment the count; at last join it as string and return the length
 def compress(chars):
         seen = set()
         result = []
         count=1
         index = 0
         while index < len(chars):
-            
-            if chars[index] in seen:
-                count+=1
-          
-                if index == len(chars)-1:
-                    if count > 1:
-                        if count<10:
-                            result.append(str(count))
-                            count = 1
-                        elif count >= 10:
-                            i = 0
-                            while count >0:
-                                count-=10
-                                i+=1
-                                result.append(str(i))
-                            count = 1
-                        seen = seen.clear()
-            elif chars[index] not in seen:
-                if count > 1:
-                    if count<10:
+            if chars[index] not in seen:
+                  if count > 1:
                         result.append(str(count))
                         count = 1
-                    elif count >= 10:
-                        i = 0
-                        while count > 0:
-                            count-=10
-                            i+=1
-                            result.append(str(i))
-                            seen = seen.clear()
-                        count+=1
-                result.append(chars[index])
-                seen.add(chars[index])
+                  seen.clear()     
+                  result.append(chars[index])
+                  seen.add(chars[index])
+            elif index == len(chars)-1:
+                  count+=1
+                  result.append(str(count))
+            else:
+                  count+=1
             index+=1
+        result = "".join(result)
 
-        return result
+        print(list(result))
+        return len(result)
 
 print(compress(["a","b","c","d","e","f","g","g","g","g","g","g","g","g","g","g","g","g","a","b","c"]))
